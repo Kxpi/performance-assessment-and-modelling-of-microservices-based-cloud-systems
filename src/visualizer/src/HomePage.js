e24a3df9fabb410541c51f151cffa06e4656c53b
matdebski <mateusz.debski@student.put.poznan.pl>
2023-10-16 05:03:24 +0200

    Add buttons

--- src/visualizer/src/HomePage.js
+++ src/visualizer/src/HomePage.js
@@ -3,22 +3,29 @@ import CallGraphPage from './components/callGraph/CallGraphPage';
 import FileUploader from './components/callGraph/components/FileUploader';
 
 
-
 function HomePage() {
-
-  const [data, setData] = useState(null)
-
+  const [data, setData] = useState(null);
+  const [showCallGraph, setShowCallGraph] = useState(true);
 
   return (
-    <div >
-
+    <div>
       <h1>Trace Visualizer</h1>
-      <FileUploader setData={setData} />
-      {data && <CallGraphPage data={data} />}
-      
 
+
+      <FileUploader setData={setData} />
+
+      {data
+        &&
+        <div>
+          <div>
+            <button onClick={() => setShowCallGraph(true)}>Show CallGraph</button>
+            <button onClick={() => setShowCallGraph(false)}>Show FelaGraph</button>
+          </div>
+          {showCallGraph ? <CallGraphPage data={data} /> : <h2>Put your graph here</h2>}
+        </div>
+      }
     </div>
   )
-}
+};
 
 export default HomePage;
