1b593a48b3c4007252aa6b966db0bfb27ebe604b
xFELAx <mifelmi@gmail.com>
2023-11-13 11:22:44 +0100

    Add react-loader-spinner to dependencies and display loading animation while uploading file

--- src/visualizer/src/components/callGraph/components/FileUploader.js
+++ src/visualizer/src/components/callGraph/components/FileUploader.js
@@ -1,24 +1,23 @@
-import React, { useCallback, useMemo, useState, useEffect } from 'react';
-import { useDropzone } from 'react-dropzone';
-import axios from 'axios';
-
-
-
+import React, { useCallback, useMemo, useState, useEffect } from "react";
+import { useDropzone } from "react-dropzone";
+import axios from "axios";
+import { ThreeDots } from "react-loader-spinner";
 
 const FileUploader = ({ setData }) => {
-
   const [fileContent, setFileContent] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Funkcja do odczytu zawartości pliku i walidacji rozszerzenia
   const handleFileDrop = useCallback((acceptedFiles) => {
+    setIsLoading(true);
     if (acceptedFiles.length === 0) {
-      alert('Błąd: Załaduj plik JSON.');
+      alert("Błąd: Załaduj plik JSON.");
       return;
     }
 
     const file = acceptedFiles[0];
-    if (file.type !== 'application/json') {
-      alert('Błąd: Plik musi mieć rozszerzenie JSON.');
+    if (file.type !== "application/json") {
+      alert("Błąd: Plik musi mieć rozszerzenie JSON.");
       return;
     }
 
@@ -28,33 +27,34 @@ const FileUploader = ({ setData }) => {
       setFileContent(content);
     };
     reader.readAsText(file);
-
   }, []);
 
   // Konfiguracja dropzone
-  const dropzoneConfig = useMemo(() => ({
-    accept: 'application/json',
-    multiple: false,
-    onDrop: handleFileDrop,
-  }), [handleFileDrop]);
+  const dropzoneConfig = useMemo(
+    () => ({
+      accept: "application/json",
+      multiple: false,
+      onDrop: handleFileDrop,
+    }),
+    [handleFileDrop]
+  );
 
-
-  const { getRootProps, getInputProps, isDragActive } = useDropzone(dropzoneConfig);
-
+  const { getRootProps, getInputProps, isDragActive } =
+    useDropzone(dropzoneConfig);
 
   useEffect(() => {
     if (fileContent) {
-
       const parsedData = JSON.parse(fileContent);
 
       //console.log(parsedData);
       axios
-        .post('http://127.0.0.1:5000/upload', parsedData, {
+        .post("http://127.0.0.1:5000/upload", parsedData, {
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         })
         .then((response) => {
+          setIsLoading(false);
           // Set the data using the 'setData' prop
           // console.log(response.data)
 
@@ -62,7 +62,7 @@ const FileUploader = ({ setData }) => {
         })
         .catch((error) => {
           console.error(error);
-          alert('Error uploading the file to the server.');
+          alert("Error uploading the file to the server.");
         });
     }
   }, [fileContent]);
@@ -70,7 +70,9 @@ const FileUploader = ({ setData }) => {
   return (
     <div {...getRootProps()} style={dropzoneStyle}>
       <input {...getInputProps()} />
-      {isDragActive ? (
+      {isLoading ? (
+        <ThreeDots type="ThreeDots" color="#00BFFF" height={80} width={80} />
+      ) : isDragActive ? (
         <p>Drop JSON file here...</p>
       ) : (
         <p>Drag & drop file here, or click to select json file</p>
@@ -81,14 +83,14 @@ const FileUploader = ({ setData }) => {
 
 // Styl dropzone, możesz dostosować go według własnych preferencji
 const dropzoneStyle = {
-  width: '75%',
-  minHeight: '150px',
-  border: '2px dashed #cccccc',
-  borderRadius: '4px',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  cursor: 'pointer',
+  width: "75%",
+  minHeight: "150px",
+  border: "2px dashed #cccccc",
+  borderRadius: "4px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  cursor: "pointer",
 };
 
 export default FileUploader;
