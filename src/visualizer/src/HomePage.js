4333467a06fad8e5d7a90fdc62d8f42b75d64c99
xFELAx <mifelmi@gmail.com>
2023-12-11 14:37:31 +0100

    Add ScatterPlotGroupsOperations to Call Graph view

--- src/visualizer/src/HomePage.js
+++ src/visualizer/src/HomePage.js
@@ -9,12 +9,24 @@ import {
   randomColors,
   processSelectedGroupData,
   processHistogramSingleGroupData,
+  processScatterPlotGroupsOperationsData,
   myColors,
 } from "./helpers.js";
 import DurationHistogramGroupsOperations from "./components/DurationHistogramGroupsOperations.jsx";
 import DurationHistogramSingleGroup from "./components/DurationHistogramSingleGroup.jsx";
 import StartTimeHistogramGroupsOperations from "./components/StartTimeHistogramGroupsOperations.jsx";
 import StartTimeHistogramSingleGroup from "./components/StartTimeHistogramSingleGroup.jsx";
+import ScatterPlotGroupsOperationsCg from "./components/ScatterPlotGroupsOperationsCg.jsx";
+import * as SVGs from "./components/SVGs";
+import { set } from "lodash";
+
+const svgComponents = Object.entries(SVGs)
+  .sort(([keyA], [keyB]) => {
+    const numA = Number(keyA.replace("Svg", ""));
+    const numB = Number(keyB.replace("Svg", ""));
+    return numA - numB;
+  })
+  .map(([, value]) => value);
 
 function HomePage() {
   const [data, setData] = useState(null);
@@ -25,6 +37,8 @@ function HomePage() {
   const [selectedGroupOperationsToParent, setSelectedGroupOperationsToParent] =
     useState(null);
   const [histogramSingleGroupData, setHistogramSingleGroupData] = useState([]);
+  const [scatterPlotGroupsOperationsData, setScatterPlotGroupsOperationsData] =
+    useState([]);
 
   const [
     isDurationHistogramGroupsOperationsVisible,
@@ -41,6 +55,10 @@ function HomePage() {
   const [
     isStartTimeHistogramSingleGroupVisible,
     setStartTimeHistogramSingleGroupVisibility,
+  ] = useState(false);
+  const [
+    isScatterPlotGroupsOperationsVisible,
+    setScatterPlotGroupsOperationsVisibility,
   ] = useState(false);
 
   // 0 -CallGraph 1 - ScatterPlot 2 - PercendanceGraph
@@ -232,6 +250,57 @@ function HomePage() {
                 Start Time Histogram of Group {selectedGroup.groupID}
               </Dropdown.Item>
             </DropdownButton>
+            <DropdownButton
+              id="dropdown-basic-button"
+              title="View Scatter Plots"
+              style={{
+                zIndex: 10010,
+                margin: 0,
+                top: 25,
+                padding: 0,
+              }}
+              onClick={() => {
+                setScatterPlotGroupsOperationsData(
+                  processScatterPlotGroupsOperationsData(
+                    selectedGroup,
+                    svgComponents
+                  )
+                );
+              }}
+            >
+              <Dropdown.Item
+                style={{
+                  zIndex: 10010,
+                  position: "relative",
+                  backgroundColor: isScatterPlotGroupsOperationsVisible
+                    ? "chartreuse"
+                    : "white",
+                }}
+                onClick={() => {
+                  setScatterPlotGroupsOperationsVisibility(
+                    !isScatterPlotGroupsOperationsVisible
+                  );
+                }}
+              >
+                Scatter Plot of Group's {selectedGroup.groupID} Operations
+              </Dropdown.Item>
+              <Dropdown.Item
+                style={{
+                  zIndex: 10010,
+                  position: "relative",
+                  backgroundColor: isStartTimeHistogramGroupsOperationsVisible
+                    ? "chartreuse"
+                    : "white",
+                }}
+                onClick={() => {
+                  setStartTimeHistogramGroupsOperationsVisibility(
+                    !isStartTimeHistogramGroupsOperationsVisible
+                  );
+                }}
+              >
+                Scatter Plot of Group's {selectedGroup.groupID} Spans
+              </Dropdown.Item>
+            </DropdownButton>
           </div>
         )}
       </div>
@@ -285,6 +354,16 @@ function HomePage() {
                   </div>
                   <StartTimeHistogramSingleGroup
                     data={histogramSingleGroupData}
+                  />
+                </div>
+              )}
+              {isScatterPlotGroupsOperationsVisible && (
+                <div>
+                  <div className="centered-text">
+                    Scatter Plot of Group's {selectedGroup.groupID} Operations
+                  </div>
+                  <ScatterPlotGroupsOperationsCg
+                    data={scatterPlotGroupsOperationsData}
                   />
                 </div>
               )}
